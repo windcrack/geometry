a702f12863a46a280712ddd8918cdb27e2c6831a
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2022-07-10 22:01:15 +0300

    fix bugs for sass and html, start add class im scroll

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const footerBlock = $('#footer').offset().top;
+const footerBlockHeight = $('#footer').height();
+
+$(window).scroll( () =>{
+    let scroll = $(window).scrollTop();
+    if(scroll > footerBlock && scroll < (footerBlock + footerBlockHeight)){
+        $('.aside-lang__call').addClass('aside-lang__call_active')
+        $('.aside-social__img').addClass('aside-social__img_active')
+    }else{
+        $('.aside-lang__call').removeClass('aside-lang__call_active')
+        $('.aside-social__img').removeClass('aside-social__img_active')
+    }
+})
+
+$(".consultation-form__input").mask("+7(999)999-99-99");
