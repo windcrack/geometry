123fdb11d5833734f70c999ad195de2cd82a89b4
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2022-07-19 11:54:54 +0300

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-import addScrollClass from './addClass'
+const mainBlock = document.querySelector('.container').offsetHeight + 2643;
+const asideCall = document.querySelector('.aside-lang__call');
+const asideSocalImg = document.querySelectorAll('.aside-social__img')
+
+function classAdd(){
+    let scroll = $(window).scrollTop();
+    if(scroll > mainBlock){
+        asideCall.classList.add('aside-lang__call_active')
+        asideSocalImg.forEach(elem => {
+            elem.classList.add('aside-social__img_active')
+        })
+
+    }else{
+        asideCall.classList.remove('aside-lang__call_active')
+        asideSocalImg.forEach(elem => {
+            elem.classList.remove('aside-social__img_active')
+        })
+    }
+}
+
+function addScrollClass(){
+    window.addEventListener('scroll', classAdd)
+}
+
 
 addScrollClass()
 
